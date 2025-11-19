2cf26ac0df11ad9efe2c03f0f33ba3e28d147dd9
zero <khalil.guimaraes@escola.pr.gov.br>
2025-11-19 09:50:46 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-function app() {
-    const[mesage, setMessage] 
+import { useState } from "react";
+
+function App(){/* cria a função componente*/
+  ////let message = 'olá mundo';/* variavel em java script*/
+  const [message, setMessage] = useState('olá, mundo'); //o set message altera o valor do state, useState tem que ter a mensagem inicial
+
+  ////state (estado) variavel que atualiza o componente
+  return (
+  <div>
+    <h1>{message}</h1>{/*jsx em ação */}
+    <button onClick={() =>{
+      setMessage ('Olá, fui clicado');
+    }}>Mudar Mensagem</button>{/*cria um botão, o java script sempre vai dentro da chaves */ }
+  </div>/* o que ela vai retornar*/
+  );
 }
+
+export default App /* exporta o app*/
